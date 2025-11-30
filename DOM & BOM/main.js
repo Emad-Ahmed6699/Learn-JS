8a9b7b6292519e44dea1492c2adaf0fd2282525a
Emad-Ahmed6699 <Emad.Elden2974@compit.aun.edu.eg>
2025-11-30 16:38:37 +0200

    inner and outer

--- DOM & BOM/main.js
+++ DOM & BOM/main.js
@@ -24,20 +24,20 @@ document.getElementsByClassName('para');// selects <-all-> elements with the cla
 console.log(document.getElementsByClassName('para')); // Output: HTMLCollection(3) [p.para, p.para, p.para] (array-like object)
 console.log(document.getElementsByClassName('para')[0]);//accessing the first element that have a class para
 document.getElementsByClassName('para')[0].innerText = "This is the updated first paragraph.";
-document.getElementsByClassName('para')[1].style.color='red';
+document.getElementsByClassName('para')[1].style.color = 'red';
 //3-getElementsByTagName
 document.getElementsByTagName('p');// selects <-all-> elements with the tag name 'p'
-document.getElementsByTagName('p')[1].style.fontWeight='bold';
+document.getElementsByTagName('p')[1].style.fontWeight = 'bold';
 
 //----------------------------------------------------------------------------------\\
 //4-querySelector ((select the first element that matches))
-let x =document.querySelector('.para');// selects the first element that matches the CSS selector '.para'(class name-->.)
-let y =document.querySelector('#head');//(id-->#)
-let z =document.querySelector('h1');//(the element)
-z.style.fontWeight='bold';//accessing any element using queryselector
+let x = document.querySelector('.para');// selects the first element that matches the CSS selector '.para'(class name-->.)
+let y = document.querySelector('#head');//(id-->#)
+let z = document.querySelector('h1');//(the element)
+z.style.fontWeight = 'bold';//accessing any element using queryselector
 //5-querySelectorAll ((select all element))
-let a =document.querySelectorAll('.para'); //that returns an array so we can access the first match using ([0]after the () )
-document.querySelectorAll('.para')[2].style.color='green';
+let a = document.querySelectorAll('.para'); //that returns an array so we can access the first match using ([0]after the () )
+document.querySelectorAll('.para')[2].style.color = 'green';
 //----------------------------------------------------------------------------------\\
 
 //accessing the body
@@ -45,7 +45,7 @@ console.log(document.body);
 //accessing the title
 console.log(document.title);
 //changing the title
-document.title="New Title";
+document.title = "New Title";
 //accessing the images
 console.log(document.images);
 console.log(document.images[0]);//accessing the first image
@@ -54,8 +54,8 @@ console.log(document.images[1].src);//accessing the src of the second images
 console.log(document.links);
 console.log(document.links[0]);//accessing the href of the first link
 console.log(document.links[1].href);//accessing the href of the second link
-document.links[1].style.color='red';//changing the color of the second link
-document.links[0].href="https://www.google.com";//changing the href of the first link
+document.links[1].style.color = 'red';//changing the color of the second link
+document.links[0].href = "https://www.google.com";//changing the href of the first link
 //accessing the forms
 console.log(document.forms);//HTMLCollection[]
 console.log(document.forms[0]);//accessing the first form
@@ -67,9 +67,9 @@ console.log(img2.src);//getting the src attribute of the image
 console.log(img2.alt);//getting the alt attribute of the image
 console.log(img2.getAttribute('src'));//getting the src attribute of the image
 //set attribute
-img2.alt="This is a new alt text";//setting a new alt attribute
-img2.setAttribute('title','This is a title attribute');//setting a new title attribute
-img2.title="This is another title attribute";//setting a new title attribute
+img2.alt = "This is a new alt text";//setting a new alt attribute
+img2.setAttribute('title', 'This is a title attribute');//setting a new title attribute
+img2.title = "This is another title attribute";//setting a new title attribute
 //remove attribute
 img2.removeAttribute('title');//removing the title attribute
 // if this element has attributes
@@ -79,3 +79,16 @@ console.log(img2.hasAttribute('title'));
 //display all attributes
 console.log(img2.attributes);
 console.log(img2.attributes[0]);
+//----------------------------------------------------------------------------------\\
+// innerhtml vs outerhtml , innertext vs outer text
+let container = document.getElementById('container');
+console.log(container.outerHTML);//return the whole element
+console.log(container.innerHTML);//return the inner element of the the element itself
+container.innerHTML+='<p>hello</p>';//ADD to it this element
+console.log(container.outerHTML);
+
+//the innertext and the outertext add text not an element
+container.innerText ='<p>hello</p>';
+console.log(container); //<div id="container"> <p>hello</p> </div> as text
+container.textContent ='<p>hello</p>';//like inner text 
+
